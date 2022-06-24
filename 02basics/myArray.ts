aa34ab8e0d097115fa1004481e7e5b63a585d4a2
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-06-24 13:43:40 +0530

    arrays in typescript

---
+++ 02basics/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string;
+    isActive: boolean
+};
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    [255, 255, 255]
+]
+
+superHeros.push("Indra");
+heroPower.push(100);
+
+allUsers.push({name: "", isActive: true});
---
+++ 02basics/myarray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    [255, 255, 255]
+];
+superHeros.push("Indra");
+heroPower.push(100);
+allUsers.push({ name: "", isActive: true });
