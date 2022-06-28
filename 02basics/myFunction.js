20fef7f8a8ad185656877c63ec1264e2d3253f3b
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-06-28 12:02:03 +0530

    function return types

--- 02basics/myFunction.js
+++ 02basics/myFunction.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 function addTwo(num) {
+    // return 'hello';
     return num + 2;
 }
 addTwo(5);
@@ -14,3 +15,24 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser('s', 's@s');
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 Ok"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["indra", "varun", "agni", "surya", "chandra", "vayu"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero, " ");
+    // return 2;
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+// error handeling
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- 02basics/myFunction.ts
+++ 02basics/myFunction.ts
@@ -1,4 +1,5 @@
-function addTwo(num: number){
+function addTwo(num: number): number{
+    // return 'hello';
     return num + 2
 }
 
@@ -17,5 +18,35 @@ signUpUser("shreyas", "shreyasmanolkar123@gmail.com", false);
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
 
 loginUser('s', 's@s');
+
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+
+//     return "200 Ok"
+// }
+
+const getHello = (s: string):string => {
+    return "";
+}
+
+const heros = ["indra", "varun", "agni", "surya", "chandra", "vayu"];
+
+heros.map((hero): string => {
+    return `hero is ${hero} `;
+    // return 2;
+});
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg);
+}
+
+// error handeling
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
+
 
 export {}
