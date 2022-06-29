cb169549fe3c2f42b13fa1199617a0acae2f3d21
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-06-29 11:43:08 +0530

    functions

---
+++ 02basics/myFunction.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("shreyas");
+function signUpUser(name, email, isPaid) { }
+signUpUser("shreyas", "shreyasmanolkar123@gmail.com", false);
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser('s', 's@s');
---
+++ 02basics/myFunction.ts
@@ -0,0 +1,21 @@
+function addTwo(num: number){
+    return num + 2
+}
+
+addTwo(5);
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+
+getUpper("shreyas");
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+signUpUser("shreyas", "shreyasmanolkar123@gmail.com", false);
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+loginUser('s', 's@s');
+
+export {}
