734930539b69feb88f4bc4e1ab7a670e6cb20646
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-07-02 11:09:40 +0530

    variable me

---
+++ 01intro/variableMe.js
@@ -0,0 +1,2 @@
+var greetings = "hello Shreyas";
+console.log(greetings);
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,8 @@
+let greetings: string = "hello Shreyas";
+console.log(greetings);
+
+let myNum: number = 4;
+
+greetings.toLocaleLowerCase();
+
+export {}
