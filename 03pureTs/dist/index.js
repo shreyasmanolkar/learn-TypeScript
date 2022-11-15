8b8640f4600887baa9cdf89204970617c9d5d8aa
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-15 09:31:20 +0530

    typescript classes

--- 03pureTs/dist/index.js
+++ 03pureTs/dist/index.js
@@ -1,10 +1,20 @@
 "use strict";
-class User {
+// class User {
+//     public email:string;
+//     private name: string;
+//     readonly city: string = "pune";
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name; 
+//     }
+// }
+// const shreyas = new User("s@s.com", "shreyas");
+// shreyas.city = "pune";
+// shreyas.city;
+class User2 {
     constructor(email, name) {
-        this.city = "pune";
         this.email = email;
         this.name = name;
     }
 }
-const shreyas = new User("s@s.com", "shreyas");
-// shreyas.city = "pune";
+const shreyas = new User2("s@s.com", "shreyas");
--- 03pureTs/src/index.ts
+++ 03pureTs/src/index.ts
@@ -1,15 +1,27 @@
-class User {
+// class User {
 
-    email:string;
-    name: string;
-    readonly city: string = "pune";
+//     public email:string;
+//     private name: string;
+//     readonly city: string = "pune";
 
-    constructor(email: string, name: string){
-        this.email = email;
-        this.name = name; 
-    }
-}
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name; 
+//     }
+// }
 
-const shreyas = new User("s@s.com", "shreyas");
+// const shreyas = new User("s@s.com", "shreyas");
 
 // shreyas.city = "pune";
+// shreyas.city;
+
+
+class User2 {
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+    ){}
+}
+
+const shreyas = new User2("s@s.com", "shreyas");
