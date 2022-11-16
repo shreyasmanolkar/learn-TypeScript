2d482fa6786a4c5634c06f4f34e54061345700ee
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-16 10:06:16 +0530

    getter and setters in typescript

--- 03pureTs/dist/index.js
+++ 03pureTs/dist/index.js
@@ -11,10 +11,35 @@
 // const shreyas = new User("s@s.com", "shreyas");
 // shreyas.city = "pune";
 // shreyas.city;
-class User2 {
-    constructor(email, name) {
-        this.email = email;
+// class User2 {
+//     constructor(
+//         public email: string, 
+//         public name: string,
+//         // private userId: string
+//     ){}
+// }
+// const shreyas = new User2("s@s.com", "shreyas");
+//  getters and setters
+class User3 {
+    constructor(name, email) {
         this.name = name;
+        this.email = email;
+        this._courseCount = 1;
+        this.city = "pune";
+    }
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNumber;
     }
 }
-const shreyas = new User2("s@s.com", "shreyas");
--- 03pureTs/src/index.ts
+++ 03pureTs/src/index.ts
@@ -16,12 +16,48 @@
 // shreyas.city;
 
 
-class User2 {
-    constructor(
-        public email: string, 
-        public name: string,
-        // private userId: string
-    ){}
-}
+// class User2 {
+//     constructor(
+//         public email: string, 
+//         public name: string,
+//         // private userId: string
+//     ){}
+// }
 
-const shreyas = new User2("s@s.com", "shreyas");
+// const shreyas = new User2("s@s.com", "shreyas");
+
+
+//  getters and setters
+
+class User3 {
+    
+    private _courseCount = 1;
+
+    readonly city: string = "pune";
+
+    constructor(
+        public name: string,
+        public email: string
+    ){
+    }
+
+    private deleteToken(){
+        console.log("Token Deleted");
+    }
+
+    get getAppleEmail(): string{
+        return `apple ${this.email}`        
+    }
+
+    get courseCount(): number{
+        return this._courseCount;
+    }
+
+    set courseCount(courseNumber: number){
+        if(courseNumber <= 1){
+            throw new Error("Course count should be more than 1"); 
+        }
+        this._courseCount = courseNumber;
+    }
+
+}
