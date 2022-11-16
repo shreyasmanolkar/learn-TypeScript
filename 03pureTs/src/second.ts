cee350ad0048f1c9a36ac9913090fa72664f2d86
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-16 10:41:12 +0530

    interfaces

---
+++ 03pureTs/src/second.ts
@@ -0,0 +1,30 @@
+interface TakePhoto {
+    cameraMode: string;
+    filter: string;
+    burst: number;    
+};
+
+interface Stories {
+    createStory(): void
+}
+
+class Instagram implements TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+};
+
+class YouTube implements TakePhoto, Stories {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: string
+    ){}
+
+    createStory(): void {
+        console.log('story is created');
+    }
+};
