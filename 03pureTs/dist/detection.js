1d4863612afd02fd51997396874c3d64df5992cc
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-11-17 00:44:47 +0530

    type predicate

--- 03pureTs/dist/detection.js
+++ 03pureTs/dist/detection.js
@@ -34,3 +34,26 @@ function isAdminAccount(account) {
         return account.isAdmin;
     }
 }
+;
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+;
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
--- 03pureTs/src/detection.ts
+++ 03pureTs/src/detection.ts
@@ -41,4 +41,29 @@ function isAdminAccount(account: User | Admin){
     if("isAdmin" in account){
         return account.isAdmin;
     }
+};
+
+function logValue(x: Date | string){
+    if(x instanceof Date){
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+};
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird){
+    if(isFish(pet)){
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird food"
+    }
 }
